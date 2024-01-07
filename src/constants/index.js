01debc6e24459a7637b17633edefd4bddf9e412a
shubham <shubham@yliway.com>
2024-01-07 13:11:43 +0530

    deployment

---
+++ src/constants/index.js
@@ -0,0 +1,258 @@
+import {
+  frontend,
+  backend,
+  ux,
+  prototyping,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  git,
+  figma,
+  docker,
+  postgresql,
+  rubyrails,
+  graphql,
+  komikult,
+  leaderboard,
+  math,
+  movie,
+  nyeusi,
+  space,
+  coverhunt,
+  differenz,
+  dcc,
+  kelhel,
+  microverse,
+  netizens,
+  yliway,
+  sql,
+  socket,
+  cloudservice,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Frontend Developer",
+    icon: frontend,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "UI/UX Design",
+    icon: ux,
+  },
+  {
+    title: "Cloud Services",
+    icon: cloudservice,
+  },
+];
+
+const technologies = [
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "graphql",
+    icon: graphql,
+  },
+  {
+    name: "postgresql",
+    icon: postgresql,
+  },
+  {
+    name: "sql",
+    icon: sql,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+  {
+    name: "socket",
+    icon: socket,
+  },
+];
+
+const experiences = [
+  {
+    title: "Web Developer",
+    company_name: "Differenz System Pvt Ltd",
+    icon: differenz,
+    iconBg: "#333333",
+    date: "Dec 2021 - Aug 2022",
+  },
+  {
+    title: "MERN Stack Developer",
+    company_name: "Netizens Technologies",
+    icon: netizens,
+    iconBg: "#333333",
+    date: "Aug 2022 - July 2023",
+  },
+  {
+    title: "Full Stack Developer",
+    company_name: "Yliway",
+    icon: yliway,
+    iconBg: "#333333",
+    date: "Oct 2023 - Present",
+  },
+];
+
+const projects = [
+  {
+    id: "project-1",
+    name: "KomiKult",
+    description: "A comic characters list app that displays Marvel characters.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: komikult,
+    repo: "https://github.com/shaqdeff/KomiKult",
+    demo: "https://shaqdeff.github.io/KomiKult/",
+  },
+  {
+    id: "project-2",
+    name: "Leaderboard",
+    description:
+      "A leaderboard list app that displays scores submitted by different players.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: leaderboard,
+    repo: "https://github.com/shaqdeff/Leaderboard",
+    demo: "https://shaqdeff.github.io/Leaderboard/",
+  },
+  {
+    id: "project-3",
+    name: "Math Magicians",
+    description: "This is a single-page calculator app built with React",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: math,
+    repo: "https://github.com/shaqdeff/Math-Magicians",
+    demo: "https://inspiring-medovik-37d3b3.netlify.app/",
+  },
+  {
+    id: "project-4",
+    name: "Movie Metro",
+    description: `A single-page application that allows users to search for any movie or show's ratings and its details.`,
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: movie,
+    repo: "https://github.com/shaqdeff/Movie-Metro",
+    demo: "https://movie-metro.netlify.app/",
+  },
+  {
+    id: "project-5",
+    name: "Nyeusi Fest Site",
+    description:
+      "This is a demo concert website for a music festival called Nyeusi.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: nyeusi,
+    repo: "https://github.com/shaqdeff/Nyeusi-Fest-Site",
+    demo: "https://shaqdeff.github.io/Nyeusi-Fest-Site/",
+  },
+];
+
+export { services, technologies, experiences, projects };
